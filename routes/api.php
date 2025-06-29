45ae8028fc02d68ef8e9a22128f5f1002883f2d9
JLipreso <lipreso.developer@gmail.com>
2025-06-29 22:49:23 +0800

    Composer updated

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'api'], function () {
     Route::group(['prefix' => 'reviews'], function () {
-        
+        Route::post('test_call', function () {
+            echo "Connected successfully";
+        });
     });
 });
 
